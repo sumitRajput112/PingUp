6f8601b44d661265f4473c114b11dedc33c987f0
sumitRajput112 <sumitrajputsr544@gmail.com>
2026-09-19 15:44:23 +0530

    fix error

--- server/models/User.js
+++ server/models/User.js
@@ -9,9 +9,9 @@ const userSchema =new mongoose.Schema({
     profile_picture:{type:String, default:''},
     cover_photo:{type:String, default:''},
     location:{type:String, default:''},
-    followers:{type:String, ref:'User'},
-    following:{type:String, ref:'User'},
-    connections:{type:String, ref:'User'},
+    followers:[{type:String, ref:'User'}],
+    following:[{type:String, ref:'User'}],
+    connections:[{type:String, ref:'User'}],
 },{timestamps:true,minimize:false})
 
 const user = mongoose.model('user',userSchema)
